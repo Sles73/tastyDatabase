d239b6525d23eee88d38cc198ee508ca2d6c00ae
Sochyno <168537554+Sochyno@users.noreply.github.com>
2024-05-02 17:54:46 +0200

    Styled search bar

---
+++ src/front/search.php
@@ -0,0 +1,39 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "users";
+
+    // Vytvoření připojení
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Kontrola připojení
+    if ($conn->connect_error) {
+        die("Připojení selhalo: " . $conn->connect_error);
+    }
+
+    // Získání hodnoty z vyhledávacího pole, pokud existuje
+    if (isset($_POST['searchBar'])) {
+        $search = $_POST['searchBar'];
+        // SQL dotaz pro vyhledávání podle názvu
+        $sql = "SELECT * FROM imgs WHERE nazev LIKE '%$search%'";
+
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            // výpis dat z každého řádku
+            echo "<ul id=\"whisper\">";
+            while($row = $result->fetch_assoc()) {
+            echo "<li>" . $row["nazev"]. "</li>";
+            }
+            echo "</ul>";
+        } else {
+            echo "0 výsledků";
+        }
+    }
+    if (isset($_POST['clear']))
+    {
+        
+    }
+    $conn->close();
+?>
