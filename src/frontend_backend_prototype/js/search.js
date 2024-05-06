6449ba15666b878f62c7aae0ea0ee5a317b059dc
Sles73 <usagisles@mail.com>
2024-05-06 16:48:45 +0200

    sorting koncept

--- src/frontend_backend_prototype/js/search.js
+++ src/frontend_backend_prototype/js/search.js
@@ -21,16 +21,21 @@ $(document).ready(function(){
     });
 
 
-    $(document).ready(function(){
-        // When the document is ready, send an AJAX request to insert a post
-        $.ajax({
-            url: 'php/insert_post.php',
-            type: 'POST',
-            success: function(response){
-                $('#result').html(response);
-            },
-            error: function(xhr, status, error){
-                console.error('AJAX Error:', status, error);
-            }
-        });
+$(document).ready(function(){
+    // When the document is ready, send an AJAX request to insert a post
+    cards_sort("");
+});
+
+function cards_sort(sorting_method){
+    $.ajax({
+        url: 'php/insert_post.php',
+        type: 'POST',
+        data: {sort: sorting_method},
+        success: function(response){
+            $('#result').html(response);
+        },
+        error: function(xhr, status, error){
+            console.error('AJAX Error:', status, error);
+        }
     });
+}
--- src/frontend_backend_prototype/php/insert_post.php
+++ src/frontend_backend_prototype/php/insert_post.php
@@ -1,24 +1,25 @@
 <?php
-// Simulate database connection and insertion
-// Replace this with your actual database connection and insertion code
 include("connect.php");
 $conn = connect();
 
-// Assuming you're using PDO for database operations
 
-$sql = "SELECT * FROM imgs
-            ORDER BY date;";                            //zástupný znak pro všechny
-            $result = $conn->query($sql);
+
+if(isset($_POST["sort"]))
+    switch($_POST["sort"]){
+        case "hodnoceni":
+            $sort = "hodnoceni DESC";
+            break;
+        default:
+            $sort = "imgID";
+            break;
+
+    }
+
+    $sql = "SELECT * FROM imgs
+    ORDER BY ".$sort." ;";                     
+    $result = $conn->query($sql);
 
     while($row = $result->fetch_assoc()){
-
-        /*echo "<div class=\"obed\"> 
-            <h2>".$row["nazev"].'</h2>
-            Dne: '.$row["date"].'<br>
-            Hodnoceno: '.$row["hodnoceni"].'/10<br>
-            Chod: '.$row["chod"].'<br>
-            <img style="height: 50vh;" src="uploads/'.$row["fileName"].'" alt="'.$row["nazev"].'">
-            </div>';*/
         echo '<div class="obed"> 
             <div id="rating">
                 <p>'.$row["hodnoceni"].'/10</p>
@@ -27,9 +28,9 @@ $sql = "SELECT * FROM imgs
             <h2>'.$row["nazev"].'</h2>
             <p>'.date("d.m. Y", strtotime($row["date"])).' &emsp; Chod: '.$row["chod"].'</p>
         </div>';
-        
-
         }
+
+
     
 
 $conn->close();
