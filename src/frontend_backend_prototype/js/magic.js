0df396bf99739f08f9b9e3eac6cde5de94340ea0
Sles73 <usagisles@mail.com>
2024-05-06 20:47:35 +0200

    it workii

--- src/frontend_backend_prototype/js/magic.js
+++ src/frontend_backend_prototype/js/magic.js
@@ -1,20 +1,22 @@
 
 $(document).ready(function(){
     // Function to load main content using AJAX
-    function loadMainContent() {
-        $.ajax({
-            url: 'main.html', // URL of the main content HTML file
-            type: 'GET',
-            success: function(response){
-                $('#vsechno').html(response); // Replace the main content with the loaded content
-            },
-            error: function(xhr, status, error){
-                console.error('AJAX Error:', status, error);
-            }
-        });
-    }
+    
 
-    loadMainContent(); // Load main content on page load
+    loadMainContent("main.html"); // Load main content on page load
 
     
 });
+
+function loadMainContent(where) {
+    $.ajax({
+        url: where, // URL of the main content HTML file
+        type: 'GET',
+        success: function(response){
+            $('#vsechno').html(response); // Replace the main content with the loaded content
+        },
+        error: function(xhr, status, error){
+            console.error('AJAX Error:', status, error);
+        }
+    });
+}
