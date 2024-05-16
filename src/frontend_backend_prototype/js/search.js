f5f80a799b7981825095dc797abbedd335206e24
Sochyno <168537554+Sochyno@users.noreply.github.com>
2024-05-16 08:08:23 +0200

    Neco s tou sirkou

--- src/frontend_backend_prototype/js/search.js
+++ src/frontend_backend_prototype/js/search.js
@@ -40,14 +40,12 @@ function cards_sort(sorting_method){
     });
 }
 
-window.onload = function()
-{
-    var horizontalniObdarovanost = document.getElementById("second");
-    var style = window.getComputedStyle(horizontalniObdarovanost);
-    var sirka = style.getPropertyValue("width");
-    var defaultValue = parseInt(sirka, 10);
-    console.log(defaultValue);
-    var rightValue = defaultValue - (defaultValue % 335) - 35;
-    console.log(rightValue);
-    horizontalniObdarovanost.style.width = rightValue + "px";
-}
+//dynamické určení velikosti barokní krásy - kurpoletního ideálu krásy
+var horizontalniObdarovanost = document.getElementById("second");
+var style = window.getComputedStyle(horizontalniObdarovanost);
+var sirka = style.getPropertyValue("width");
+var defaultValue = parseInt(sirka, 10);
+console.log(defaultValue);
+var rightValue = defaultValue - (defaultValue % 335) - 35;
+console.log(rightValue);
+horizontalniObdarovanost.style.width = rightValue + "px";
