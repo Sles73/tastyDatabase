b7a268fc21fe4887c23c0c50ab27eea81e8e438a
Sles73 <usagisles@mail.com>
2024-06-11 22:47:16 +0200

    calling python from php example

--- src/examples/script.py
+++ src/examples/script.py
@@ -12,7 +12,7 @@ for i in range(0,9):
     chodyDiv = jidelnicekDen1.find_all("div")[1]
 
     for x in range(3, 6):
-        print("Obed ", x - 2)
+        print("<br>Obed ", x - 2)
         obedDiv = chodyDiv.find_all("div")[x]
         obedDivContent = obedDiv.get_text()
         strednik = obedDivContent.index(';')
@@ -20,7 +20,7 @@ for i in range(0,9):
         zavorka2 = obedDivContent.rfind('(')
         obed = obedDivContent[strednik + 2:zavorka2].strip()
         print(obed)
-    print('\n')
+    print('<br>')
 
 
 
