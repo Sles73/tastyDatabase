413a9fa1e88fb7781ebd58eca5afb3db2dbeb2c4
Sochyno <168537554+Sochyno@users.noreply.github.com>
2024-06-11 18:06:51 +0200

    iCanteen webscrape example

---
+++ src/examples/script.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://strava.spseplzen.cz/faces/login.jsp"
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
+
+for i in range(0,9):
+    jidelnicekDen1 = doc.find_all("div", {"class": "jidelnicekDen"})[i]
+    den = jidelnicekDen1.find("span", {"class": "important"})
+    print(den.string.strip())
+    chodyDiv = jidelnicekDen1.find_all("div")[1]
+
+    for x in range(3, 6):
+        print("Obed ", x - 2)
+        obedDiv = chodyDiv.find_all("div")[x]
+        obedDivContent = obedDiv.get_text()
+        strednik = obedDivContent.index(';')
+        zavorka1 = obedDivContent.index('(')
+        zavorka2 = obedDivContent.rfind('(')
+        obed = obedDivContent[strednik + 2:zavorka2].strip()
+        print(obed)
+    print('\n')
+
+
+
