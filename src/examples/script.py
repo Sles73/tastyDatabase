ee484b40cd2edfb2d0609c2a66b17afba23eb3be
Sles73 <usagisles@mail.com>
2024-06-12 09:02:02 +0200

    windows scraper

--- src/examples/script.py
+++ src/examples/script.py
@@ -5,14 +5,14 @@ url = "https://strava.spseplzen.cz/faces/login.jsp"
 result = requests.get(url)
 doc = BeautifulSoup(result.text, "html.parser")
 
-for i in range(0,9):
+for i in range(0,8):
     jidelnicekDen1 = doc.find_all("div", {"class": "jidelnicekDen"})[i]
     den = jidelnicekDen1.find("span", {"class": "important"})
     print(den.string.strip())
     chodyDiv = jidelnicekDen1.find_all("div")[1]
 
     for x in range(3, 6):
-        print("<br>Obed ", x - 2)
+        print("Obed ", x - 2)
         obedDiv = chodyDiv.find_all("div")[x]
         obedDivContent = obedDiv.get_text()
         strednik = obedDivContent.index(';')
@@ -20,7 +20,6 @@ for i in range(0,9):
         zavorka2 = obedDivContent.rfind('(')
         obed = obedDivContent[strednik + 2:zavorka2].strip()
         print(obed)
-    print('<br>')
 
 
 
---
+++ src/examples/webscraper_FUCKINGWINDOWS.php
@@ -0,0 +1,15 @@
+<?php
+// Specify the path to the Python executable and the script
+$python = "C:\\Path\\To\\Python\\python.exe";
+$script = "C:\\Path\\To\\Your\\Script\\script.py";
+
+// Prepare the command with arguments
+$arg = "John";
+$command = escapeshellcmd("$python $script $arg");
+
+// Execute the command and capture the output
+$output = shell_exec($command . ' 2>&1');
+
+// Display the output
+echo $output;
+?>
